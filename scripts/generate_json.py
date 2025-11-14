b74075259c4550aabf14148abf193782c06d3cc7
Stasia Michalska <hel@lcp.world>
2025-11-14 03:09:58 +0100

    Only do operations on yaml if it's not empty

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -31,29 +31,30 @@ def generate_locations(infile, outfile):
 
 def generate_events(infile, outfile):
     yaml = load_yaml_from_file(infile)
-    sorted(yaml, key=lambda d: d['start_date'])
     output = {}
-    for event in yaml:
-        current_month = [event['start_date'].year, event['start_date'].month]
-        end_month = [event['end_date'].year, event['end_date'].month]
-        while True:
-            calendar_string = '%04d-%02d' % (current_month[0], current_month[1])
+    if yaml:
+        sorted(yaml, key=lambda d: d['start_date'])
+        for event in yaml:
+            current_month = [event['start_date'].year, event['start_date'].month]
+            end_month = [event['end_date'].year, event['end_date'].month]
+            while True:
+                calendar_string = '%04d-%02d' % (current_month[0], current_month[1])
 
-            if not calendar_string in output:
-                output[calendar_string] = []
+                if not calendar_string in output:
+                    output[calendar_string] = []
 
-            output[calendar_string].append(event)
+                output[calendar_string].append(event)
 
-            if current_month != end_month:
-                if current_month == 12:
-                    current_month = [event.start_date.year + 1, 1]
+                if current_month != end_month:
+                    if current_month == 12:
+                        current_month = [event.start_date.year + 1, 1]
+                    else:
+                        current_month = [event.start_date.year, event.start_date.month + 1]
                 else:
-                    current_month = [event.start_date.year, event.start_date.month + 1]
-            else:
-                break
+                    break
 
-    for date in output:
-        write_json_to_file(output[date], outfile % date)
+        for date in output:
+            write_json_to_file(output[date], outfile % date)
 
 
 def generate_404(contents, outfile):
